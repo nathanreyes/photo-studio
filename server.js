c8893274db26a134c714404f2fd531c440d90b1e
Nathan Reyes <nathanreyes@me.com>
2016-11-11 11:35:32 -0600

    Change default port to 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 // Create server ==================
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
